2f6f093dbb1fc6d8f0dc47ef0e2d7061eee97c20
yudafhd <yuda@Ahmads-MacBook-Pro.local>
2025-11-04 16:08:43 +0700

    update status version

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   manifest: {
     name: 'Bookmark Saga',
     description: 'New Tab showcasing your bookmark and latest browsing history.',
-    version: '1.6.4',
+    version: '1.6.5',
     permissions: ['history', 'storage', 'tabs', 'bookmarks'],
 
     icons: {
